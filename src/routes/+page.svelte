3230fa54775b75068124c88f96eeeb6aefb6a01e
peleg2210 <45825608+pkd2210@users.noreply.github.com>
2025-07-20 01:04:16 +0300

    First rev

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<div class="absolute inset-0 -z-10 h-full w-full bg-orange-100 bg-[linear-gradient(to_right,#80808010_1px,transparent_1px),linear-gradient(to_bottom,#80808020_1px,transparent_1px)] bg-[size:20px_20px]">
+<div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div>
+</div>
+<div class="flex flex-col items-center p-4 sm:p-22 min-h-screen justify-between">
+<div></div>
+<div class="flex flex-col items-center px-4">
+	<h1 class="text-4xl sm:text-6xl md:text-8xl text-amber-500 font-oi text-center" style="font-family: 'Oi'">DUMMIES</h1>
+    <div class="h-1 bg-gradient-to-r from-transparent via-amber-500 to-transparent my-4 sm:my-8 mx-auto w-3/4"></div>
+	<button class="px-4 py-3 sm:px-6 sm:py-6 bg-amber-500 text-white rounded-lg font-extrabold hover:scale-105 transform transition-transform text-sm sm:text-base">RSVP HERE!</button>
+</div>
+
+<div class="pt-4 sm:pt-8">
+	<p class="text-gray-600 text-sm sm:text-base text-center">Scroll down</p>
+	<div class="text-gray-600 text-xl sm:text-2xl animate-bounce text-center">
+		↓
+	</div>
+</div>
+
+
+</div>
+<div class="relative flex flex-col items-center p-4 sm:p-22 min-h-screen">
+<div class="absolute inset-0 -z-10 h-full w-full bg-gray-50 bg-[linear-gradient(to_right,#00000000_1px,transparent_1px),linear-gradient(to_bottom,#80808010_1px,transparent_1px)] bg-[size:20px_20px]">
+<div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div>
+</div>
+
+<h2 class="text-3xl sm:text-4xl md:text-6xl py-6 sm:py-12 text-amber-500 font-oi text-center px-4" style="font-family: 'Oi'">WHAT ARE WE DOING?</h2>
+<div class="text-amber-700 text-center px-4">were</div>
+
+</div>
