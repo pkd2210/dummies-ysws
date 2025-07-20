f1bc92e1a6d807adbee2a9073b0a505e85bb2c53
peleg2210 <45825608+pkd2210@users.noreply.github.com>
2025-07-20 15:42:05 +0300

    made most of the styling parts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,51 @@
-<div class="absolute inset-0 -z-10 h-full w-full bg-orange-100 bg-[linear-gradient(to_right,#80808010_1px,transparent_1px),linear-gradient(to_bottom,#80808020_1px,transparent_1px)] bg-[size:20px_20px]">
+<div class="absolute inset-0 -z-10 h-full w-full bg-orange-100">
 <div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div>
 </div>
 <div class="flex flex-col items-center p-4 sm:p-22 min-h-screen justify-between">
 <div></div>
 <div class="flex flex-col items-center px-4">
-	<h1 class="text-4xl sm:text-6xl md:text-8xl text-amber-500 font-oi text-center" style="font-family: 'Oi'">DUMMIES</h1>
-    <div class="h-1 bg-gradient-to-r from-transparent via-amber-500 to-transparent my-4 sm:my-8 mx-auto w-3/4"></div>
-	<button class="px-4 py-3 sm:px-6 sm:py-6 bg-amber-500 text-white rounded-lg font-extrabold hover:scale-105 transform transition-transform text-sm sm:text-base">RSVP HERE!</button>
-</div>
+	<div class="relative flex items-center justify-center">
+		<img src="/images/For Dummies logo.png" alt="Logo" class="absolute w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 object-contain -translate-y-8 sm:-translate-y-12 md:-translate-y-15 -translate-x-35 sm:-translate-x-32 md:-translate-x-85 rotate-325 hover:scale-125 transition-transform duration-300"/>
+		<h1 class="text-4xl sm:text-6xl md:text-8xl text-amber-500 font-oi text-center" style="font-family: 'Oi'">DUMMIES</h1>
+	</div>
 
+    <div class="h-1 bg-gradient-to-r from-transparent via-amber-500 to-transparent my-4 sm:my-8 mx-auto w-3/4"></div>
+	<div class="flex flex-col sm:flex-row gap-4 items-center">
+		<button class="shadow-lg/20 px-4 py-3 sm:px-6 sm:py-7 bg-amber-500 text-white rounded-lg font-extrabold hover:scale-105 transform transition-transform text-sm sm:text-base">SIGN UP</button>
+		<button class="shadow-lg/20 px-4 py-3 sm:px-6 sm:py-7 bg-amber-500 text-white rounded-lg font-extrabold hover:scale-105 transform transition-transform text-sm sm:text-base">SUBMIT PROJECT</button>
+	</div>
+</div>
+<a href="#WAWD">
 <div class="pt-4 sm:pt-8">
 	<p class="text-gray-600 text-sm sm:text-base text-center">Scroll down</p>
 	<div class="text-gray-600 text-xl sm:text-2xl animate-bounce text-center">
 		↓
 	</div>
 </div>
+</a>
 
 
 </div>
-<div class="relative flex flex-col items-center p-4 sm:p-22 min-h-screen">
-<div class="absolute inset-0 -z-10 h-full w-full bg-gray-50 bg-[linear-gradient(to_right,#00000000_1px,transparent_1px),linear-gradient(to_bottom,#80808010_1px,transparent_1px)] bg-[size:20px_20px]">
+<div class="relative flex flex-col items-center p-4 sm:p-22">
+<div class="absolute inset-0 -z-10 h-full w-full bg-gray-50">
 <div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div>
 </div>
 
-<h2 class="text-3xl sm:text-4xl md:text-6xl py-6 sm:py-12 text-amber-500 font-oi text-center px-4" style="font-family: 'Oi'">WHAT ARE WE DOING?</h2>
-<div class="text-amber-700 text-center px-4">were</div>
+<h2 id="WAWD" class="text-3xl sm:text-4xl md:text-6xl py-6 sm:py-12 text-amber-500 font-oi text-center px-4" style="font-family: 'Oi'">WHAT ARE WE DOING?</h2>
+<div class="py-8 text-amber-700 text-lg sm:text-2xl md:text-4xl text-center px-2" style="font-family: 'Kirang Haerang'"><strong>*You*</strong> Learn A New programming language, and build a project with it<br><strong>*We*</strong> Give you a book of choice from <u><a href=https://www.dummies.com target="_blank">dummies.com</a></u></div>
 
 </div>
+
+<div class="relative flex flex-col items-center p-4 sm:p-22">
+<div class="absolute inset-0 -z-10 h-full w-full bg-amber-200">
+<div class="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-fuchsia-400 opacity-20 blur-[100px]"></div>
+</div>
+
+<h2 class="text-3xl sm:text-4xl md:text-6xl py-6 sm:py-12 text-amber-500 font-oi text-center px-4" style="font-family: 'Oi'">FAQ</h2>
+<div class="py-8 text-amber-700 text-lg sm:text-2xl md:text-4xl text-center px-2" style="font-family: 'Kirang Haerang'">
+<strong><li>What Count as a New proggraming lanuage?</li></strong><br>Any proggraming language that you never tried to learn, and never used for any project<br><br><br>
+<strong><li>Can i double dip with SOM?</li></strong><br>Answear Soon...<br><br><br>
+<strong><li>What the time requirment?</li></strong><br>At least 10 hours<br><br><br>
+</div>
+</div>
+<div class="text-center text-gray-600 text-sm mt-8 pb-4">Made with &lt;3 by <a href="https://github.com/pkd2210" target="_blank" class="text-amber-600 hover:text-amber-700 underline">pkd2210</a></div>
